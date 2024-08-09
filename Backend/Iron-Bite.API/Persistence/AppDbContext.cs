248907e9aa6547930ec6ffa7c4d95300223f4773
Kamil Fudala <kamil.fudala44@gmail.com>
2024-08-09 08:00:33 +0200

    Set properties for decimal fields.

--- Backend/Iron-Bite.API/Persistence/AppDbContext.cs
+++ Backend/Iron-Bite.API/Persistence/AppDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Iron_Bite.API.Entities;
+using Iron_Bite.API.Models;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Iron_Bite.API.Persistence;
 
@@ -9,4 +11,29 @@ public class AppDbContext(DbContextOptions options) : DbContext(options), IAppDb
     public DbSet<Meal> Meals { get; set; } = null!;
     public DbSet<Nutrient> Nutrients { get; set; } = null!;
     public DbSet<IngredientMeal> IngredientMeals { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        modelBuilder.Entity<Nutrient>()
+            .Property(n => n.Carbohydrates)
+            .HasPrecision(5, 2); 
+
+        modelBuilder.Entity<Nutrient>()
+            .Property(n => n.Fats)
+            .HasPrecision(5, 2); 
+
+        modelBuilder.Entity<Nutrient>()
+            .Property(n => n.Protein)
+            .HasPrecision(5, 2);
+
+        modelBuilder.Entity<IngredientMeal>()
+            .OwnsOne(i => i.Quantity, q =>
+            {
+                q.Property(p => p.Value)
+                    .HasPrecision(5, 2);
+            });
+    }
 }
