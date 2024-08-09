ca43c0aab3285618639333cdbd8a55f22e0cb6d8
Kamil Fudala <kamil.fudala44@gmail.com>
2024-08-09 07:47:50 +0200

    Create AppDbContext and add connection string.

---
+++ Backend/Iron-Bite.API/Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Iron_Bite.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iron_Bite.API.Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options), IAppDbContext
+{
+    public DbSet<Ingredient> Ingredients { get; set; } = null!;
+    public DbSet<Meal> Meals { get; set; } = null!;
+    public DbSet<Nutrient> Nutrients { get; set; } = null!;
+    public DbSet<IngredientMeal> IngredientMeals { get; set; } = null!;
+}
---
+++ Backend/Iron-Bite.API/Persistence/IAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Iron_Bite.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iron_Bite.API.Persistence;
+
+public interface IAppDbContext
+{
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Meal> Meals { get; set; }
+    public DbSet<Nutrient> Nutrients { get; set; }
+    public DbSet<IngredientMeal> IngredientMeals { get; set; }
+}
